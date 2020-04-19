c60b994a6c97b514b0589efaee4065a3dae19cd0
Diego Paredes <many@iMac-de-Diego.local>
2020-04-18 23:58:53 -0500

    Manejando Eventos

--- tutos/hello-react-next/src/Product.js
+++ tutos/hello-react-next/src/Product.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 class Product extends React.Component {
+    showPrice() { // Método para mostrar el precio del producto
+        alert(this.props.product.price);
+    }
+
     render() { // PROPS vienen desde ProductList
-        return <li>
+        return <li onClick={ () => this.showPrice() }>
             <h3>{ this.props.product.name }</h3>
             <p>{ this.props.product.description }</p>
         </li>;
